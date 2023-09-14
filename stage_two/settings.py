80398435a8f73a0422070ff1cb10d4e6f6b4636c
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-14 01:57:11 +0100

    Add whitenoise

--- stage_two/settings.py
+++ stage_two/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(k6cjxx9c%oo#vqf%wjg#s@sxg8=3%c1%%=fm(u6v+s_afp430'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'delight-hng.onrender.com']
 
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
