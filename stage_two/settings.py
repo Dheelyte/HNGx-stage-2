079932bb9a065c0ecf98e31b52587c7ca3d236ea
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2023-09-14 02:52:26 +0100

    Debug false

--- stage_two/settings.py
+++ stage_two/settings.py
@@ -145,9 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
